24c38fac8ce9b5fece4064bf3e64690905155601
Ai Tran <youraiball@gmail.com>
2023-04-09 23:10:38 -0700

    redid whole lab

--- game.py
+++ game.py
@@ -2,29 +2,61 @@
 
 import random
 
-print("Hello there!")
-name = input("What is your name? ")
-print(f"{name}, I'm thinking of a number between 1 and 100.")
-number = random.randint(1,100)
-guess =  None
-tries = 0
+name = input("Hello, let's play a number guessing game. What is your name? ")
 
-while guess != number:
-    while True:
+while True:
+    print(f"{name}, I need a range for the secret number.")
+    start = int(input("What is the start of your range? "))
+    end = int(input("What is the end of your range? "))
+    print(f" Ok {name}, I am thinking of a number between {start} and {end}.")
+    number = random.randint(start,end)
+    guess = None
+    tries = 0
+    best_score = 0
+    max_tries = 5
+
+    while guess != number:
+        if max_tries == 0:
+            print("You've reached the limit of tries")
+            break
+
         try:
             guess = int(input("What is your guess? "))
-            break
-        except ValueError:
-            print("Nice try but no. That's not an integer. Try again.")
+        except (TypeError, ValueError):
+            print("You must enter an integer.")
+            continue
 
-    if guess > 100 or guess < 1:
-        print("Did you read? That is not within range. Please try again.")
+        if guess < start or guess > end:
+            print(f"Thats not within range. Stay between {start} - {end} and guess again")
+            continue
+
+        tries += 1
+        max_tries -= 1
+
+        if guess > number:
+            print("Your guess is too large. Guess again")
+        elif guess < number:
+            print("Your guess is too small. Guess again")
+        elif guess == number:
+            # Check if current number of tries is less than the current best score
+            # If yes, set best score to current number of tries
+            # Account for edge case of first round where best_score is 0
+            if best_score == 0:
+                best_score = tries
+            elif tries < best_score:
+                best_score = tries
+            print(f"Congrats! You guessed my number in {tries} tries!")
+        
+    play_again = input("Would you like to play again? Y/N ").upper()
+    if play_again == "Y":
         continue
-
-    tries += 1
-    if guess > number:
-        print("Your guess is too large. Please try again.")
-    elif guess < number:
-        print("Your guess is too low. Please try again.")
+    elif play_again == "N":
+        print(f"Thank you for playing. Your best score is {best_score} tries.")
+        break
     else:
-        print(f"Bingo! You guessed the number in {tries} tries!")
+        print("That's a yes-or-no question. Y or N ?")
+        guess = number
+
+
+        
+            
